b449bc6126a28750e501ddbb9f8ff4bb181274d5
rahmonali2009 <mahamadalivrahmonali@gmail.com>
2024-03-16 03:41:48 -0700

    first commit

---
+++ src/pages/firstpages.jsx
@@ -0,0 +1,240 @@
+import React from 'react'
+import '../css/nav.css'
+export default function firstpages() {
+  return (
+    <div className='header'>              
+                                                                <nav>
+                                                                  <div className='q1'>
+                                                                <img src="https://lebazar.uz/static/2eb900388c6cde74af2241893dc8dc54.svg" alt="" />
+
+                                                                </div>
+                                                                <div id='cx1'></div>
+                                                                <div className='q2'>
+                                                                <i class="fa-solid fa-magnifying-glass"></i>
+                                                                <i class="fa-solid fa-heart"></i>
+                                                                <i class="fa-solid fa-cart-shopping"></i>
+                                                                <input type="image" className='w1' src="https://th.bing.com/th/id/OIP.b_EeulcPQto-CYoOZ-_RiQHaHa?w=164&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7" alt="" />
+
+
+                                                                <input  className='a2' type="text" placeholder='Vontiy' />
+                                                                </div>
+                                                                </nav>
+
+                                                                                                                          <div className='qs1'>
+                                                                                                                          <div className='as1'>
+                                                                                                                            <div className='q11'>
+                                                                                                                              
+                                                                                                                          <h1 id='q1'>Lebazar GOni yuklab oling!</h1>
+                                                                                                                          </div>
+                                                                                                                          <br />
+                                                                                                                          <div className='q12'>
+                                                                                                                          <h2 className='q13'>Lebazar GO ilovasida qulay interfeys, to‘liq</h2>
+                                                                                                                          <br />
+                                                                                                                          <h2 className='q13'>assortiment, qulay qidiruv, buyurtma holatini va</h2>
+                                                                                                                          <br />
+                                                                                                                          <h2 className='q13'>yetkazuvchini yo‘lini kuzatish mavjud. Narxlar va</h2>
+                                                                                                                          <br />
+                                                                                                                          <h2 className='q13'>chegirmalar Korzinkadek. Eng muhimi esa - 3 ta</h2>
+
+                                                                                                                          <br />
+                                                                                                                          <h2 className='q13'>buyurtmaning yetkazib berish xizmati bepul bo‘ladi!</h2>
+
+
+
+
+
+                                                                                                                          </div>
+                                                                                                                          <div className='x1'>
+                                                                                                                          <img className='x2' src="https://lebazar.uz/static/2928664fe1fc6aca88583a6f606d60ba.svg" alt="" />
+
+                                                                                                                          <img className='zq' src="https://lebazar.uz/static/1e91d02cf5a902f38f2923c006d79281.png" alt="" />
+                                                                                                                          <img className='zq1' src="https://lebazar.uz/static/7e2f6450c5050d9072d3bb1622d90e56.png" alt="" />
+                                                                                                                          </div>
+
+
+                                                                                                                          </div>
+
+
+                                                                                                                          <div className='as2'>
+                                                                                                                          <img className='x3' src="https://lebazar.uz/static/9d4e7d3b5ad607d2a4f9e451348c343f.jpg" alt="" />
+
+
+                                                                                                                          </div>
+
+
+                                                                                                                          </div>
+
+                                                                                                                          <div className='x11'>
+                                                                                                                          <div  className='x12'>
+                                                                                                                          <h1 className='x13'>Ilovaning afzalliklari</h1>
+                                                                                                                          </div>
+                                                                                                                          <div className='x14'>
+                                                                                                                          <div className='x15'>
+                                                                                                                          <img className='x19' src="https://lebazar.uz/static/b2d29ab369371d796faefa5c0bd061be.png" alt="" />
+                                                                                                                          <h3 className='x20'>3 ta yetkazib berish bepul</h3>
+                                                                                                                          </div>
+
+                                                                                                                          <div className='x15'>
+                                                                                                                          <img  className='x19'  src="https://lebazar.uz/static/3c9328a3acf4776c041067dc88f60cf6.png" alt="" />
+                                                                                                                          <h3  className='x20'>Eng yaxshi assortiment</h3>
+                                                                                                                          </div>
+
+                                                                                                                          <div className='x15'>
+                                                                                                                          <img className='x19' src="https://lebazar.uz/static/f1ae1070f07a3d659fd828dd490e63a8.png" alt="" />
+                                                                                                                          <h3 className='x20'  >Tez va qulay qidiruv</h3>
+
+                                                                                                                          </div>
+
+                                                                                                                          <div className='x15'>
+                                                                                                                            <img className='x19' src="https://lebazar.uz/static/0ee8ab09f4ce4af711da37675c9ebddf.png" alt="" />
+                                                                                                                          <h3 className='x20'>Oson buyurtma berish</h3>
+                                                                                                                          </div>
+
+                                                                                                                          </div>
+                                                                                                                          <div className='az'>
+                                                                                                                          <div className='azas111'>
+                                                                                                                          <h1>Darkstordan uyingizga</h1>
+                                                                                                                          </div>
+                                                                                                                          <div className='aza1'>
+                                                                                                                          <div className='aza2'>
+                                                                                                                          <img className='m1' src="https://lebazar.uz/static/0381b0c1dcb5309423d02a5b1c574389.png" alt="" />
+                                                                                                                          <h2 className='m2'>Darkstor - bu</h2>
+                                                                                                                          <h3 className='m3' >onlayn xarid uchun maxsus ombor do‘koni</h3>
+                                                                                                                          </div>
+                                                                                                                          <div className='aza2'>
+                                                                                                                          <img  className='m1' src="https://lebazar.uz/static/d5b5d8263ea21c3a2c4fb1ebfa1fd005.png" alt="" />
+                                                                                                                          <h2 className='m2' >To'liq assortiment</h2>
+                                                                                                                          <h3 className='m3' >ilovadagi mahsulotlar doim mavjud</h3>
+                                                                                                                          </div>
+                                                                                                                          <div className='aza2'>
+                                                                                                                          <img className='m1' src="https://lebazar.uz/static/1b6c6325c321165378ea369a783a631d.png" alt="" />
+                                                                                                                          <h2 className='m2'>Yuqori sifat</h2>
+                                                                                                                          <h3 className='m3'>uchun saqlash sharoitlari nazorat qilinadi</h3>
+                                                                                                                          </div>
+
+
+                                                                                                                          </div>
+
+                                                                                                                          </div>
+
+
+
+
+
+
+
+
+                                                                                                                          <div className='qs1'>
+                                                                                                                          <div className='as1'>
+                                                                                                                            <div className='q112'>
+                                                                                                                          <h1 id='q2'>Lebazar GOni yuklab oling!</h1>
+                                                                                                                          </div>
+                                                                                                                          <br />
+                                                                                                                          <div className='q12'>
+                                                                                                                          <h2 className='q13'>Lebazar GO ilovasida qulay interfeys, to‘liq</h2>
+                                                                                                                          <br />
+                                                                                                                          <h2 className='q13'>assortiment, qulay qidiruv, buyurtma holatini va</h2>
+                                                                                                                          <br />
+                                                                                                                          <h2 className='q13'>yetkazuvchini yo‘lini kuzatish mavjud. Narxlar va</h2>
+                                                                                                                          <br />
+                                                                                                                          <h2 className='q13'>chegirmalar Korzinkadek. Eng muhimi esa - 3 ta</h2>
+
+                                                                                                                          <br />
+                                                                                                                          <h2 className='q13'>buyurtmaning yetkazib berish xizmati bepul bo‘ladi!</h2>
+
+
+
+
+
+                                                                                                                          </div>
+                                                                                                                          <div className='x1'>
+                                                                                                                          <img className='x2' src="https://lebazar.uz/static/2928664fe1fc6aca88583a6f606d60ba.svg" alt="" />
+
+                                                                                                                          <img className='zq' src="https://lebazar.uz/static/1e91d02cf5a902f38f2923c006d79281.png" alt="" />
+                                                                                                                          <img className='zq1' src="https://lebazar.uz/static/7e2f6450c5050d9072d3bb1622d90e56.png" alt="" />
+                                                                                                                          </div>
+
+
+                                                                                                                          </div>                                                                      
+
+
+                                                                                                                          <div className='as2'>
+                                                                                                                          <img className='x3' src="https://lebazar.uz/static/9d4e7d3b5ad607d2a4f9e451348c343f.jpg" alt="" />
+
+
+                                                                                                                          </div>
+
+                                                                                                                                                                                               
+                                                                                                                          </div >
+
+
+                                                                                                                          <div className='zaf'>
+
+      <div className='m01'>
+      <div className='m02'>
+      <div className='m03'>
+      <h3>Yetkazib berish va to'lov
+      Ommaviy oferta
+      Siyosiy hujjatlar
+      </h3>
+      </div>
+      <div className='m04'>
+      <h5>
+      Barcha savollaringiz bo’yicha o'zingizga ma’qul paytda, bizga qo'ng'iroq qiling:  +998 71 200-11-12
+      </h5>
+      </div>
+      <div className='m05'>
+      <h5>
+      Qo'llab-quvvatlash Telegram chat: @lebazar_go_support_bot
+
+
+      </h5>
+      </div>
+      <div className='m06'>
+      <h5>
+      2016 Internet-do’kon LeBazar.uz: Oziq-ovqat mahsulotlari, xo’jalik mollari, va boshqalar.
+      </h5>
+      </div>
+      <div className='m07'>
+      <h5>
+      Mahsulotni yetkazib berish Toshkent hududida amalga oshiriladi. Barcha huquqlar himoyalangan.
+      </h5>
+      </div>
+      </div>
+
+<div className='sd1'>
+<img className='sd2' src="https://lebazar.uz/static/08788020d35864febb3fc50e810590db.png" alt="" />
+<h5 className='cv'>Ilovamizni o’rnating hamda
+  <br />
+   xaridlarni oson amalga oshiring!</h5>
+   <img className='cv1' src="https://lebazar.uz/static/2928664fe1fc6aca88583a6f606d60ba.svg" alt="" />
+   <img className='cv2' src="https://lebazar.uz/static/f104dcfa3ec774571e45ba6e6a50b0ef.svg" alt="" />
+</div>
+</div>
+
+
+  
+</div>  
+
+
+
+
+
+
+
+                                                                                                                                   </div>
+
+
+
+
+
+
+
+
+
+                                                                                                                                   
+    </div>
+  )
+}
+ 
+
